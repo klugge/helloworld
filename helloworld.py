471d7190825dbc854a948cec70a24cf5e0443ea6
klugge <i.klugge@yandex.ru>
2017-04-14 20:30:15 +0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+p = 0
+for x in 'Hello, world!'[::-1]:
+  p = p*88 + (ord(x) -32)
+
+pp = p
+p = 384468986177509059152608
+
+while p > 0:
+  print(chr(p%88 +32), end='')
+  p //= 88
+print('')
+
+def execfile(fname):
+  exec(open(fname).read())
+
+
+
+
+
+
+
